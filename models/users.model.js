b4ee9866ac06e65b67cd1be315935fa299855c70
usman672 <usmansarwar672@gmail.com>
2020-01-02 15:39:59 -0800

    9

---
+++ models/users.model.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+const Schema = mongoose.Schema;
+
+//mongoCon =mongodb+srv://admin:11222211@cluster0-yq2ve.mongodb.net/admin?retryWrites=true&w=majority
+
+const User = new Schema({
+    id: {
+        type: Number,
+        unique: true,
+        sparse:true
+    },
+     name: {
+        type: String
+    },
+     email: {
+        type: String,
+        unique: true,
+        sparse:true
+
+    },
+     password: {
+        type: String
+    },
+    role: {
+        type: String
+    },
+    department: {
+        type: String
+    },
+    designation: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    avatar_ext: {
+        type: String
+    },
+  
+    is_deleted: {
+        type: Number,
+        default: 0
+    }
+});
+
+User.plugin(mongoosePaginate);
+
+User.methods.toJSON = function() {
+    var obj = this.toObject();
+    delete obj.password;
+    return obj;
+   }
+// User.index({'$**': 'text'});
+
+module.exports = mongoose.model("User", User);
